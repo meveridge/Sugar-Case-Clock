d7d4fc592dd2714e4017d77760c387d3873d649c
meveridge <mark.ev@gmail.com>
2014-02-07 23:51:24 -0800

    Make Config Write Cleaner
    Config Write is now simpler and cleaner.

--- writeConfig.php
+++ writeConfig.php
@@ -6,39 +6,8 @@
     
 	$conf = new Configurator();
 
-	$overrideArray = $conf->readOverride();
-	$conf->previous_sugar_override_config_array = $overrideArray;
-	$diffArray = deepArrayDiff($conf->config, $sugar_config);
-    $overrideArray = sugarArrayMerge($overrideArray, $diffArray);
-
-	// To remember checkbox state
-	if(isset($overrideArray['authenticationClass']) && empty($overrideArray['authenticationClass'])) {
-		unset($overrideArray['authenticationClass']);
-	}
-
-	//Add custom config overrides
-	$overrideArray['caseStatusExternal'] = array('Pending','Closed','Rejected','Duplicate');
-	$overrideArray['caseStatusInternal'] = array('New','Assigned','Pending Input');
-
-	//Convert Overrides to String to write to file
-	$overideString = "<?php\n/***CONFIGURATOR***/\n";
-
-	foreach($overrideArray as $key => $val) {
-		if (in_array($key, $this->allow_undefined) || isset ($sugar_config[$key])) {
-			if (is_string($val) && strcmp($val, 'true') == 0) {
-				$val = true;
-				$this->config[$key] = $val;
-			}
-			if (is_string($val) && strcmp($val, 'false') == 0) {
-				$val = false;
-				$this->config[$key] = false;
-			}
-		}
-		$overideString .= override_value_to_string_recursive2('sugar_config', $key, $val);
-	}
-	$overideString .= '/***CONFIGURATOR***/';
-
-	//Write to config_override.php
-	$conf->saveOverride($overideString);
+	$conf->config['caseStatusExternal'] = array('Pending','Closed','Rejected','Duplicate');
+	$conf->config['caseStatusInternal'] = array('New','Assigned','Pending Input');
+	$conf->handleOverride();
 
 ?>
