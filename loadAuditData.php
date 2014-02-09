9769495f110f8b5372cb9ac9aa44c7be2ab9a96c
Patrick McQueen <pmcqueen@sugarcrm.com>
2014-02-08 22:22:34 -0800

    Includes code that generates field data
    Includes function file containing functions used in the updated
    loadAuditData file, outputting four field values; two durations in
    hours, and two corresponding percentages

--- loadAuditData.php
+++ loadAuditData.php
@@ -2,6 +2,7 @@
 
 require_once('include/modules.php');
 require_once('modules/Audit/Audit.php');
+require_once('custom/modules/Cases/scc-functions.php');
 
 global $focus;
 
@@ -11,13 +12,95 @@ global $focus;
 
 //Get single Case's Audit:
 
-$focus = BeanFactory::getBean("Cases", "45b60855-49b4-b4eb-dc77-52f5d2cc8800");
+$focus = BeanFactory::getBean("Cases", "62859f2e-7bc1-6aff-fefc-52f6f541138b");
 
 $audit_list =  Audit::get_audit_list();
 
-echo "<pre>".print_r($audit_list, true)."</pre>";
-
 //Patricks code here:
+$CaseCreated = $focus->getFieldValue('date_entered');
+
+foreach(array_reverse($audit_list) as $key => $value) {
+
+	if($value['field_name'] == 'Status:') {
+
+            if ($key === 0) {
+                
+                //begin Diff New to First Status Change
+                $datetime1 = strtotime($CaseCreated);
+                $datetime2 = strtotime($value['date_created']);
+
+                //BEGIN DAYS BETWEEN 1
+                $start = new DateTime($CaseCreated);
+                $end = new DateTime($value['date_created']);
+
+                //'days between' function
+                $days = DaysBetween($daylength, $weekbegins, $weekends, $start, $end);
+
+                //differences
+                $dur1 = DiffHours($daybegins , $dayends , $daylength , $start, $end, $days);
+                //END DAYS BETWEEN 1
+
+                $prev_date = $value['date_created'];
+                
+                //adds this duration to appropriate bin arrays //
+                if ($value['before_value_string'] == 'New' || $value['before_value_string'] == 'Internal' || $value['before_value_string'] == 'Assigned') {
+                    $int_dur += $dur1;
+                }
+                else {
+                    $ext_dur += $dur1;
+                }
+                $total_dur += $dur1;
+
+                //end Diff New to First Status Change //
+
+            } else {
+                //begin get previous Status Change Time//
+                if ($prev_date !== null) {
+                    $datetime1 = strtotime($prev_date);
+                    $datetime2 = strtotime($value['date_created']);
+                    $dur1 = round(($datetime2-$datetime1)/60/60,2);
+
+                //BEGIN DAYS BETWEEN 2
+                $start = new DateTime($prev_date);
+                $end = new DateTime($value['date_created']);
+
+                //'days between' function
+                $days = DaysBetween($daylength, $weekbegins, $weekends, $start, $end);
+
+                //differences
+                $dur1 = DiffHours($daybegins , $dayends , $daylength , $start, $end, $days);
+                //END DAYS BETWEEN 2
+
+                }
+                $prev_date = $value['date_created'];
+                
+                //adds this duration to appropriate bins //
+                if ($value['before_value_string'] == 'New' || $value['before_value_string'] == 'Internal' || $value['before_value_string'] == 'Assigned') {
+                    $int_dur += $dur1;
+                }
+                else {
+                    $ext_dur += $dur1;
+                }
+                $total_dur += $dur1;
+                //end get previous Status Change Time//
+
+            }
+    }
+}
+
+//Divides durations from total to product a percentage //
+if ($total_dur != 0) 
+    {
+        $ud_percent = round(($int_dur/$total_dur)*100,1);
+        $cd_percent = round(($ext_dur/$total_dur)*100,1);
+    } else {
+        $ud_percent = 0;
+        $cd_percent = 0;
+    }
+
+echo "<pre>".print_r($int_dur, true)." >> ".print_r($ext_dur, true)."</pre>";
+echo "<pre>".print_r($ud_percent, true)." >> ".print_r($cd_percent, true)."</pre>";
+
 
 //Save time results to the bean
 
---
+++ scc-functions.php
@@ -0,0 +1,110 @@
+<?php
+
+//CONFIG INFO
+
+//Note: 06 = 6am, 18 = 6pm
+$daybegins = '06';
+$dayends = '18';
+if ($dayends > $daybegins) {
+    $daylength = $dayends - $daybegins;
+}else {
+    $daylength = ($dayends+24) - $daybegins;
+}
+
+//Note: 1 = Monday, 7 = Sunday
+$weekbegins = '1';
+$weekends = '6';
+
+$prev_date = null;
+
+//durations belonging to users and clients
+$int_dur = 0;
+$ext_dur = 0;
+$total_dur = 0;
+
+//FUNCTIONS
+
+//Excludes weekends and calculates the days between two dates
+function DaysBetween($daylength, $weekbegins, $weekends, $start, $end) {
+
+$oneday = new DateInterval("P1D");
+
+$days = array();
+$data = $daylength;
+//$data = '12';
+
+/* Iterate from $start up to $end+1 day, one day in each iteration.
+   We add one day to the $end date, because the DatePeriod only iterates up to,
+   not including, the end date. */
+$modstart = $start->format('Y-m-d H:i:s');
+$modstart = strtotime($modstart);
+$modstart = strtotime("+1 day", $modstart);
+$modstart = date("Y-m-d", $modstart);
+if ($start->format('Y-m-d') == $end->format('Y-m-d') || $modstart == $end->format('Y-m-d')) {
+    foreach(new DatePeriod($start, $oneday, $end) as $day) {
+        $days = AddDay($days, $day, $data, $weekbegins, $weekends);
+    }
+} else {
+    foreach(new DatePeriod($start, $oneday, $end->add($oneday)) as $day) {
+        $days = AddDay($days, $day, $data, $weekbegins, $weekends);
+    }
+}
+return $days;
+}
+
+//Used within DaysBetween() to add weekdays to $days
+function AddDay($days, $day, $data, $weekbegins, $weekends) {
+        $day_num = $day->format("N"); /* 'N' number days 1 (mon) to 7 (sun) */
+        if ($weekends > $weekbegins && $day_num >= $weekbegins) {
+            if($day_num < $weekends) { /* weekday */
+                array_push($days,$data);
+            }
+        } else {
+            if($day_num > $weekends && $day_num <= $weekbegins) { /* weekday */
+                array_push($days,$data);
+            }
+        } 
+        return $days;
+}
+
+//Determines the number of hours between two DateTimes
+function DiffHours($daybegins , $dayends , $daylength , $start, $end, $days) {
+
+$startstring = $start->format('Y-m-d H:i:s');
+$startend1 = $start->format('Y-m-d') . ' ' . $dayends . ':00:00';
+$ds = sizeof($days) - 1;
+$endstring = $end->format('Y-m-d H:i:s');
+$endend1 = $end->format('Y-m-d') . ' ' . $daybegins . ':00:00';
+if ($start->format('Y-m-d') !== $end->format('Y-m-d')) {
+    //for $start, determines hours between DateTime and end of day (Note: Initially, 6-18 workday is hardcoded)
+    if ($start->format('H') >= $daybegins && $start->format('H') < $dayends) {
+        $days[0] = round((strtotime($startend1) - strtotime($startstring))/60/60,2);
+    } elseif ($start->format('H') < $daybegins) {
+        $days[0] = $daylength;
+    } elseif ($start->format('H') >= $dayends) {
+        $days[0] = '00';
+    }
+
+    //for $end, determines hours between DateTime and end of day (Note: Initially, 6-18 workday is hardcoded)
+    if ($end->format('H') >= $daybegins && $end->format('H') < $dayends) {
+        $days[$ds] = round((strtotime($endstring) - strtotime($endend1))/60/60,2);
+    } elseif ($end->format('H') < $daybegins) {
+        $days[$ds] = $daylength;
+    } elseif ($end->format('H') >= $dayends) {
+        $days[$ds] = '00';
+    }
+
+} else {
+    $days[0] = '00';
+    $days[1] = round((strtotime($endstring)-strtotime($startstring))/60/60,2);
+}
+
+//computes hours between start and end
+$dur1 = 0;
+foreach ($days as $x) {
+    $dur1 = $dur1 + $x;
+}
+return $dur1;
+}
+
+?>
